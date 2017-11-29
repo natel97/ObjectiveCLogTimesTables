e0192caed7e6f327a6e8c74260ebf2fe7cf3133d
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-11-28 21:26:07 -0600

    did stuff

--- Onjective C/ViewController.h
+++ Onjective C/ViewController.h
@@ -10,6 +10,16 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *BeginA;
+@property (weak, nonatomic) IBOutlet UITextField *EndA;
+@property (weak, nonatomic) IBOutlet UITextField *BeginB;
+@property (weak, nonatomic) IBOutlet UITextField *EndB;
+@property (weak, nonatomic) IBOutlet UILabel *output;
+@property int bA;
+@property int eA;
+@property int bB;
+@property int eB;
+
 
 @end
 
--- Onjective C/ViewController.m
+++ Onjective C/ViewController.m
@@ -19,6 +19,39 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)SendToLog:(id)sender {
+    NSMutableArray *myArray = [[NSMutableArray alloc]init];
+    _bA =  [_BeginA.text intValue];
+    _eA =  [_EndA.text intValue];
+    _bB =  [_BeginB.text intValue];
+    _eB =  [_EndB.text intValue];
+    
+    if (_eA < _bA || _eB < _bB){
+        _output.text = [NSString stringWithFormat:(@"Please check input! %d %d %d %d"), _bA, _eA, _bB, _eB];
+        return;
+    }
+    
+    
+    for(int x = _bA; x <= _eA; x++){
+        [myArray addObject:[NSString stringWithFormat:(@"\n\n***Row containing %d * x***"), x]];
+        for(int y = _bB; y <= _eB; y++){
+            [myArray addObject:[NSString stringWithFormat:(@"%d * %d = %d"),x,y,x*y]];
+        }
+    }
+   NSAlert *alert = [NSAlert alertWithMessageText: @"Results!"
+                                     defaultButton:@"OK" alternateButton:@"Cancel"
+                                       otherButton:nil informativeTextWithFormat:
+                      @"Deleted records cannot be restored."];
+
+    
+    NSLog(@"%@",myArray.debugDescription);
+    _output.text = @"Check the log for the magic!;\nReady to go again!";
+    _bA = 0;
+    _bB = 0;
+    _eA = 0;
+    _eB = 0;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
